f4b79937eea862c889b1fef95350af05f73ae00c
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2020-08-28 15:44:00 +0200

    Add Command class

---
+++ src/Entity/Command.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommandRepository::class)
+ */
+class Command
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+    
+    /**
+     * getId
+     *
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    
+    /**
+     * getCreatedAt
+     *
+     * @return DateTimeInterface|null
+     */
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+    
+    /**
+     * setCreatedAt
+     *
+     * @param  DateTimeInterface $createdAt
+     * @return self
+     */
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
